4fc509e974ff9b116b789e1f1fb5a5efbbc3f4b2
pushpendra <dubeypushpendra@gmail.com>
2020-07-30 09:12:20 +0530

    Deletcontactlist

--- mycontentlist.py
+++ mycontentlist.py
@@ -41,9 +41,20 @@ def Editcontact():
     with open('E:\\project\\Python\\pymycontactlist\\contactlist.txt', "w") as file:
         file.write(newlines)
 
+def Deletcontactlist():
+    name = input("Enter name to search: ")
+    newlines = ""
+    with open('E:\\project\\Python\\pymycontactlist\\contactlist.txt') as file:
+        lines = file.readlines()
+        for line in lines:
+            if not line.__contains__(name):
+                newlines = newlines + line
+    with open('E:\\project\\Python\\pymycontactlist\\contactlist.txt', "w") as file:
+                file.write(newlines)
 
 
-yourCondition = input("What you want to do:\n 1: Show all Contacts\n2: Search Contact\n3: Add new contact\n4: Edit new contact\n==> ")
+
+yourCondition = input("What you want to do:\n 1: Show all Contacts\n2: Search Contact\n3: Add new contact\n4: Edit new contact\n5: Delete new contact\n==> ")
 
 if yourCondition == "1":
     ShowAllContacts()
@@ -53,3 +64,5 @@ if yourCondition == "3":
     AddNewContact()
 if yourCondition == "4":
     Editcontact()
+if yourCondition == "5":
+    Deletcontactlist()
